01d716ef6537a6fd636a12cca013edc56b747c04
harsh-yuvraj20 <yuvraj301harsh@gmail.com>
2026-02-19 10:08:46 +0530

    UC5: Add stack-based palindrome checker

--- src/u5app.java
+++ src/u5app.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
-public class u5app {
+public class U{
+
+    public static void main(String[] args) {
+        String input = "Radar";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println("The string '" + input + "' is a palindrome.");
+        } else {
+            System.out.println("The string '" + input + "' is NOT a palindrome.");
+        }
+    }
 }
