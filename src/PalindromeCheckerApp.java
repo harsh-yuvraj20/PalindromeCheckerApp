720570d411b593e1214f7881896656f2e17d4021
harsh-yuvraj20 <yuvraj301harsh@gmail.com>
2026-02-26 11:25:59 +0530

    Add UC6: Queue + Stack based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,34 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String inputUC5 = "Radar";
-        Stack<Character> stack = new Stack<>();
+        String inputUC6 = "Radar";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stackUC6 = new Stack<>();
 
-        for (int i = 0; i < inputUC5.length(); i++) {
-            stack.push(inputUC5.charAt(i));
+        for (int i = 0; i < inputUC6.length(); i++) {
+            char ch = Character.toLowerCase(inputUC6.charAt(i));
+            queue.add(ch);
+            stackUC6.push(ch);
         }
 
-        String reversedUC5 = "";
-        while (!stack.isEmpty()) {
-            reversedUC5 += stack.pop();
+        boolean isPalindromeUC6 = true;
+
+        while (!queue.isEmpty() && !stackUC6.isEmpty()) {
+            if (!queue.remove().equals(stackUC6.pop())) {
+                isPalindromeUC6 = false;
+                break;
+            }
         }
 
-        if (inputUC5.equalsIgnoreCase(reversedUC5)) {
-            System.out.println("The string '" + inputUC5 + "' is a palindrome.");
+        if (isPalindromeUC6) {
+            System.out.println("The string '" + inputUC6 + "' is a palindrome.");
         } else {
-            System.out.println("The string '" + inputUC5 + "' is NOT a palindrome.");
+            System.out.println("The string '" + inputUC6 + "' is NOT a palindrome.");
         }
 
     }
