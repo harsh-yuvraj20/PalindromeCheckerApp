67b5265464ede935c4c54de96216cf0fe1b44329
harsh-yuvraj20 <yuvraj301harsh@gmail.com>
2026-02-26 10:41:36 +0530

    Add UC1: Application entry & welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,11 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String appName = "Palindrome Checker App";
+        String appVersion = "v1.0.0";
+        System.out.println("================================");
+        System.out.println("Welcome to " + appName);
+        System.out.println("Version: " + appVersion);
+        System.out.println("================================");
+        System.out.println("Ready to process data...");
+    }
 }
