008d073f56a9bebc38211e43827b07cc1bb6ac09
harsh-yuvraj20 <reddyvinaykumarreddy206@gmail.com>
2026-03-13 08:55:19 +0530

    Implemented UC7: Deque-Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,34 +1,31 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-public class PalindromeCheckerApp {
+public class PalindromeCheckerAppUC7 {
 
     public static void main(String[] args) {
 
-        String inputUC6 = "Radar";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stackUC6 = new Stack<>();
+        String inputUC7 = "Radar";
+        Deque<Character> deque = new ArrayDeque<>();
 
-        for (int i = 0; i < inputUC6.length(); i++) {
-            char ch = Character.toLowerCase(inputUC6.charAt(i));
-            queue.add(ch);
-            stackUC6.push(ch);
+        for (int i = 0; i < inputUC7.length(); i++) {
+            char ch = Character.toLowerCase(inputUC7.charAt(i));
+            deque.addLast(ch);
         }
 
-        boolean isPalindromeUC6 = true;
+        boolean isPalindromeUC7 = true;
 
-        while (!queue.isEmpty() && !stackUC6.isEmpty()) {
-            if (!queue.remove().equals(stackUC6.pop())) {
-                isPalindromeUC6 = false;
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindromeUC7 = false;
                 break;
             }
         }
 
-        if (isPalindromeUC6) {
-            System.out.println("The string '" + inputUC6 + "' is a palindrome.");
+        if (isPalindromeUC7) {
+            System.out.println("The string '" + inputUC7 + "' is a palindrome.");
         } else {
-            System.out.println("The string '" + inputUC6 + "' is NOT a palindrome.");
+            System.out.println("The string '" + inputUC7 + "' is NOT a palindrome.");
         }
 
     }
